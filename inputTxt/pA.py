9d35d47248ffa0bf3523c74137b5af1438d0a0bf
aarkin002 <aarkin002@gmail.com>
2024-03-25 15:47:45 +0300

    inputTxt
    реализован

---
+++ inputTxt/pA.py
@@ -0,0 +1,21 @@
+with open('input.txt') as f:
+    tmp = f.readlines()
+    input_arr = tmp[0].split(" ")
+
+print("Кол-во слов (в тексте есть перенос строки - он посчитан) = " + str(len(input_arr)))
+
+big_word = input_arr[0]
+
+
+for i in input_arr:
+    if len(big_word) < len(i):
+        big_word = i
+
+print("Одно из самых длинных слов (последнее в списке слово длиннее которого нет) = " + big_word)
+
+result = {}
+for word in input_arr:
+    result[word] = result.get(word, 0) + 1
+
+print("Кол-во вхождений каждого слова: ")
+print(result)
